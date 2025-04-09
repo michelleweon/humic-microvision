a8bf245309d66fd7abcbb292efe7b638e32f0eb5
Justin Lu <https://github.com/juslu28/CS50-final-project.git>
2025-04-09 17:37:51 -0400

    front-end progress

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static('./'));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
